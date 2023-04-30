20fc75ed752163ab8d31a919449f5887955563fb
root <root@proyecto1.yketr1qy1ebuxbqnmaxjaesxke.bx.internal.cloudapp.net>
2023-04-30 17:16:26 +0000

    vpn-chef-repo

---
+++ vpn/chef-repo/cookbooks/openvpn/metadata.rb
@@ -0,0 +1,18 @@
+name              'openvpn'
+version           '7.0.13'
+maintainer        'Sous Chefs'
+maintainer_email  'help@sous-chefs.org'
+license           'Apache-2.0'
+description       'Installs and configures openvpn and includes rake tasks for managing certs.'
+source_url        'https://github.com/sous-chefs/openvpn'
+issues_url        'https://github.com/sous-chefs/openvpn/issues'
+chef_version      '>= 15.3'
+
+supports 'arch'
+supports 'centos'
+supports 'debian'
+supports 'fedora'
+supports 'redhat'
+supports 'ubuntu'
+
+depends 'yum-epel'
